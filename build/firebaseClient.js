194fbadaeb353be02740211c0e61095b534d2d62
brunfernandez98 <brunfernandezrod@hotmail.com>
2022-09-20 22:15:34 -0300

    Se sube back a vercel2

--- build/firebaseClient.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("firebase/app");
-const serviceAccount = require('./serviceAccount.json');
-const app = (0, app_1.initializeApp)(serviceAccount);
-exports.default = app;
-//# sourceMappingURL=firebaseClient.js.map
