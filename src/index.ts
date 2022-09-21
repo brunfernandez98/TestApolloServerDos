2a5e453a27866d1a5b303f13f9ac249a81742361
brunfernandez98 <brunfernandezrod@hotmail.com>
2022-09-21 00:19:50 -0300

    Se sube express

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,25 @@
-import { ApolloServer } from "apollo-server";
-import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
-
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import resolvers from "./resolvers";
 import typeDefs from "./schema/typeDefs";
+import http from "http";
 
-
-
-
-
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  csrfPrevention: true,
-  cache: 'bounded',
-  introspection: true,
-  context: ({ req, res }:any) => {
-    return {
-      user: req,
-      req,
-    };
-},
-
-  /**
-   * What's up with this embed: true option?
-   * These are our recommended settings for using AS;
-   * they aren't the defaults in AS3 for backwards-compatibility reasons but
-   * will be the defaults in AS4. For production environments, use
-   * ApolloServerPluginLandingPageProductionDefault instead.
-  **/
-  plugins: [
-    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-  ],
-});
-
-// The `listen` method launches a web server.
-server.listen({port: process.env.PORT || 4000}).then(({ url, }: any) => {
-  console.log(`asdsf Server ready at ${url}`);
-});
+async function startApolloServer(schema: any, resolvers: any) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+    //tell Express to attach GraphQL functionality to the server
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  }) as any;
+  await server.start(); //start the GraphQL server.
+  server.applyMiddleware({ app });
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve) //run the server on port 4000
+  );
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+//in the end, run the server and pass in our Schema and Resolver.
+startApolloServer(typeDefs, resolvers);
