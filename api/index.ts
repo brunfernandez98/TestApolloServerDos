b721d3673635ca82db1102ed27c9f3646d6259d5
brunfernandez98 <brunfernandezrod@hotmail.com>
2022-10-06 20:25:51 -0300

    feat(lang):fdsfdsf

--- api/index.ts
+++ api/index.ts
@@ -43,7 +43,7 @@ async function startApolloServer(typeDefs: any, resolvers: any) {
 
   // Modified server startup
   await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  console.log(`🚀 Servedsdr ready at http://localhost:4000${server.graphqlPath}`);
 }
 
 startApolloServer(typeDefs, resolvers);
