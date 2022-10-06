09417c77fb170607fe2551ad17cc39a174bf839a
brunfernandez98 <brunfernandezrod@hotmail.com>
2022-10-06 20:57:34 -0300

    fdsfdsf

--- api/index.ts
+++ api/index.ts
@@ -9,44 +9,22 @@ import typeDefs from "./schema/typeDefs";
 import http from "http";
 import cors from "cors";
 
-async function startApolloServer(typeDefs: any, resolvers: any) {
-  // Required logic for integrating with Express
-  const app = express();
-  // Our httpServer handles incoming requests to our Express app.
-  // Below, we tell Apollo Server to "drain" this httpServer,
-  // enabling our servers to shut down gracefully.
-  app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
-  const httpServer = http.createServer(app);
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
 
-  // Same ApolloServer initialization as before, plus the drain plugin
-  // for our httpServer.
+const startApolloServer = async(app:any, httpServer:any) => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    csrfPrevention: true,
-    cache: 'bounded',
-    plugins: [
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-    ],
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
-  // More required logic for integrating with Express
   await server.start();
-  server.applyMiddleware({
-    app,
-
-    // By default, apollo-server hosts its GraphQL endpoint at the
-    // server root. However, *other* Apollo Server packages host it at
-    // /graphql. Optionally provide this to match apollo-server.
-    path: '/graphql'
-  });
-
-  // Modified server startup
-  await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Servedsdr asdready at http://localhost:4000${server.graphqlPath}`);
+  server.applyMiddleware({ app });
 }
 
 startApolloServer(typeDefs, resolvers);
+
+export default httpServer;
