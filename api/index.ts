705ebeb3fcdf08928e22f192ef8342c50ee910ea
brunfernandez98 <brunfernandezrod@hotmail.com>
2022-10-06 21:11:20 -0300

    fdsfds

--- api/index.ts
+++ api/index.ts
@@ -1,30 +1,30 @@
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server';
 import {
-  ApolloServerPluginDrainHttpServer,
-  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageLocalDefault,
 } from 'apollo-server-core';
 import resolvers from "./resolvers";
 import typeDefs from "./schema/typeDefs";
 import http from "http";
 import cors from "cors";
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
+  /**
+   * What's up with this embed: true option?
+   * These are our recommended settings for using AS;
+   * they aren't the defaults in AS3 for backwards-compatibility reasons but
+   * will be the defaults in AS4. For production environments, use
+   * ApolloServerPluginLandingPageProductionDefault instead.
+  **/
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+  ],
+});
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-const httpServer = http.createServer(app);
-
-const startApolloServer = async(app:any, httpServer:any) => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-
-  await server.start();
-  server.applyMiddleware({ app });
-}
-
-startApolloServer(typeDefs, resolvers);
-
-export default httpServer;
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
